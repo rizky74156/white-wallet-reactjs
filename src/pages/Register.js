5755acc5848531f1b8f843cc30a591b5ecb2b58d
Rizky Nur Fitriansyah <Rizkyackerman@gmail.com>
2022-10-22 18:02:27 +0700

    register add

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -2,56 +2,84 @@ import React from "react";
 import Back from "../asset/arrowback.svg";
 import { Link } from "react-router-dom";
 import iconregister from "../asset/register.svg";
-import Logininput from "../components/Logininput";
+// import Logininput from "../components/Logininput";
 // import Buttonregister from "../components/Buttonregister";
-import { useState } from "react";
-// import { useEffect } from "react";
 import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const Register = () => {
-  // const [akun, setAkun] = useState([]);
+  const [contact, setContacts] = useState([]);
+
+  // const [isUpdate, setIsUpdate] = useState({ id: null, status: false });
+
   const [formData, setFormData] = useState({
+    // name: "",
+    // telp: "",
     Phonenumber: "",
     Email: "",
     Fullname: "",
     Password: "",
   });
 
-  // useEffect(() => {
-  //   axios.get("http://localhost:3000/akun").then((res) => {
-  //     console.log(res.data);
-  //     setAkun(res?.data ?? []);
-  //   });
-  // }, []);
+  useEffect(() => {
+    //megambil data
+    axios.get("http://localhost:3000/akun").then((res) => {
+      console.log(res.data);
+      setContacts(res?.data ?? []);
+    });
+  }, []);
 
-  // function handleChange(e) {
-  //   let data = { ...formData };
-  //   data[e.target.name] = e.target.value;
-  //   setFormData(data);
+  function handleChange(e) {
+    let data = { ...formData };
+    data[e.target.name] = e.target.value;
+    setFormData(data);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    let data = [...contact];
+
+      let newData = {
+        // id: uid(),
+        Phonenumber: formData.Phonenumber,
+        Fullname: formData.Fullname,
+        Email: formData.Email,
+        Password: formData.Password,
+      };
+      data.push(newData);
+
+      axios.post("http://localhost:3000/akun", newData).then((res) => {
+        alert("Berhasil menyimpan data");
+      });
+
+    // Menmbah contact
+    setContacts(data);
+    // setFormData({ Phonenumber: "", Fullname: "", Email: "", Password: "" });
+    // setIsUpdate({ id: null, status: false });
+  }
+
+  // function handleEdit(id) {
+  //   let data = [...contact];
+  //   let foundData = data.find((contact) => contact.id === id);
+  //   setFormData({
+  //     Fullname: foundData.Fullname,
+  //     Phonenumber: foundData.Phonenumber,
+  //     Email: foundData.Email,
+  //     Password: foundData.Password,
+  //   });
+  //   setIsUpdate({ id: id, status: true });
   // }
 
-  function handleSubmit(e)  {
-    // let data = [...akun];
-    e.preeventDefault();
+  // function handleDelete(id) {
+  //   let data = [...contact];
+  //   let filterData = data.filter((contact) => contact.id !== id);
+  //   setContacts(filterData);
 
-    // let newData = {
-    //   Phonenumber: formData.Phonenumber,
-    //   Fullname: formData.Fullname,
-    //   Email: formData.Email,
-    //   Password: formData.Password,
-    // };
-
-    // data.push(newData);
-
-    axios.post("http://localhost:3000/akun", formData).then((res) => {
-      console.log(res);
-    }).catch((err) => {
-      console.log(err)
-    });
-
-    // setAkun(data);
-    // setFormData({ name: "", telp: "" });
-  }
+  //   axios.delete(`http://localhost:3000/akun/${id}`).then((res) => {
+  //     alert("Data berhasil dihapus");
+  //   });
+  // }
 
   return (
     <div>
@@ -68,64 +96,52 @@ const Register = () => {
           <h1>Register</h1>
         </div>
 
-        <form onSubmit={handleSubmit}>
+        <form action="" onSubmit={handleSubmit}>
           <div className="register">
             <div className="py-2">
-              <Logininput
-                label={"Phone Number"}
-                type="telp"
-                id={"phonenumber"}
-                placeholder="masukan no telefon"
-                name={"Phonenumber"}
-                change={(e) =>
-                  setFormData({ ...formData, Phonenumber: e.target.value })
-                }
-                values={formData.Phonenumber}
-              />
-            </div>
-            <div className="py-2">
-              <Logininput
-                label={"Email"}
-                type="email"
-                id={"email"}
-                placeholder="masukan email"
-                name={"Email"}
-                change={(e) =>
-                  setFormData({ ...formData, Email: e.target.value })
-                }
-                values={formData.Email}
-              />
-            </div>
-            <div className="py-2">
-              <Logininput
-                label={"Fullname"}
+              <label htmlFor="">Email</label>
+              <input
                 type="text"
-                id={"name"}
-                placeholder="masukan nama"
-                name={"Fullname"}
-                change={(e) =>
-                  setFormData({ ...formData, Fullname: e.target.value })
-                }
-                values={formData.Fullname}
+                className="form-control"
+                name="Email"
+                value={formData.Email}
+                onChange={handleChange}
               />
             </div>
             <div className="py-2">
-              <Logininput
-                label={"Password"}
+              <label htmlFor="">Full Name</label>
+              <input
+                type="text"
+                className="form-control"
+                name="Fullname"
+                value={formData.Fullname}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="py-2">
+              <label htmlFor="">No. Telp</label>
+              <input
+                type="text"
+                className="form-control"
+                name="Phonenumber"
+                value={formData.Phonenumber}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="py-2">
+              <label htmlFor="">Password</label>
+              <input
                 type="password"
-                id={"password"}
-                name={"Password"}
-                placeholder="masukan password"
-                change={(e) =>
-                  setFormData({ ...formData, Password: e.target.value })
-                }
-                values={formData.Password}
+                className="form-control"
+                name="Password"
+                value={formData.Password}
+                onChange={handleChange}
               />
             </div>
 
             <div className="py-2">
               <button type="submit" className="btn btn-primary w-100 mt-3">
-                Submit
+                Save
               </button>
             </div>
           </div>
